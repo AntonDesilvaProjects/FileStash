505b08d13417b43079408358578d576d3faf548c
Anton Desilva <antondesilva09@yahoo.com>
2017-03-08 22:43:11 -0500

    Integrated Jquery to display file upload progress

--- app/controller/ActionPanelController.js
+++ app/controller/ActionPanelController.js
@@ -8,11 +8,52 @@ Ext.define('FileStash.controller.ActionPanelController',{
 	},
 	onSelectFileBtnClick : function( fileSelectInput, value, eOpts)
 	{
+
+		var uploadForm = $('#filefield-1025-button-fileInputEl')[0];
+		var formData = new FormData();
+		formData.append('uploadItem', uploadForm.files[0] );
+
+		$.ajax({
+			xhr: function() {
+   				 var xhr = new window.XMLHttpRequest();
+
+   				 xhr.upload.addEventListener("progress", function(evt) {
+			      if (evt.lengthComputable) {
+			        var percentComplete = evt.loaded / evt.total;
+			        percentComplete = parseInt(percentComplete * 100);
+			        console.log(percentComplete);
+
+			        if (percentComplete === 100) {
+			        		alert('file uploaded !');
+			        }
+
+			      }
+			    }, false);
+
+			    return xhr;
+			 },
+			url : 'http://localhost:3000/content/upload',
+			data : formData,
+			processData : false,
+			contentType : false,
+			type : 'POST',
+			sucess : function( data )
+			{
+				console.log('success');
+				console.log(data);
+			},
+			error : function( data )
+			{
+				console.log('failure');
+				console.log(data);
+			}
+		});
+
 		//alert('in here');
 		//this.parentView.addFileUpload('C://File');
 		//Call the form submit method here
-		this.uploadForm.getForm().submit({
-			url : 'http://localhost:3000/content/upload',
+		/*this.uploadForm.getForm().submit({
+			url : ,
 			failure : function(form, response)
 			{
 				alert('failure');
@@ -23,7 +64,7 @@ Ext.define('FileStash.controller.ActionPanelController',{
 			{
 				alert('sucess');
 			}
-		});
+		});*/
 		//Attach a callback, once successful, add progress bar
 	},
 	onCollapse : function()
